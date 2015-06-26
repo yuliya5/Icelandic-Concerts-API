5cfc656386680f4c688db42d9573552cb08330a6
yuliya5 <ysuvorava@gmail.com>
2015-06-25 18:28:56 -0600

    Complete API for Icelandic Concerts Data

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 var app = angular.module('app', []);
 
-app.controller('MyCtrl', ['$scope', '$http', function ($scope, $http) {
-	$http.get('http://apis.is/earthquake/is').
-    success(function(data, status, headers, config) {
-      $scope.earthquakes = data.results;
-    }).
-    error(function(data, status, headers, config) {
-      // log error
-    });
+app.controller('MyCtrl', ['$scope', '$http', function ($scope, $http, $q) {
+	// $http.get('http://apis.is/concerts').
+ //    success(function(data, status, headers, config) {
+ //      $scope.concerts = data.name;
+
+ //    })
+
+
+    $scope.getConcert = function(){
+    	// var deferred = $q.defer();
+    	$http({
+	    	method: 'GET', 
+	        url: 'http://apis.is/concerts',
+	  		// dataType: 'json',
+	  		// success: function(response) {
+	    // 		console.log(response);
+  			// }
+    	})
+    	.then(function(concertData){
+    		// deferred.resolve(concertData);
+    		console.log(concertData)
+    		$scope.concerts = concertData.data.results;
+    	})
+    	// return deferred.promise;
+    }
+
+
+    $scope.getConcert(); //this automatically displays all the pictures in the window
+
 }])
+
+
+
+
+
--- spec.js
+++ spec.js
@@ -2,9 +2,15 @@
 describe('Protractor exercise', function() {
   var nav = element(by.css('.nav'));
   var navOptions = element.all(by.css('.nav-option'));
+  var well = element(by.css('.well'));
+  
 
   beforeEach(function() {
     browser.get('http://localhost:8080/');
+  });
+
+  it('should have a title', function(){
+    expect(browser.getTitle()).toEqual('Icelandic Concerts');
   });
 
   it('should have a nav', function() {
@@ -15,7 +21,14 @@ describe('Protractor exercise', function() {
     expect(navOptions.count()).toBe(3);
   });
 
-  it('should have a final option that says "Messages"', function(){
-    expect(navOptions.last().getText()).toContain('Messages');
-  })
+  it('should have a well separating elements', function(){
+    expect(well.isPresent()).toBe(true);
+  });
+
+  it('has the first option named "Concerts', function(){
+
+    expect(navOptions.first().getText()).toBe('Concerts');
+  });
+
+
 });
